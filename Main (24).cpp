dae5a5f1c9872cc92ce1de3391eb0e9e77998fce
Sumit mhalaskar <sumit.mhalaskar24@sanjivani.edu.in>
2024-09-23 20:41:44 +0530

    Add files via upload
    A teacher wants to buy supplies for her classroom. She needs 15 notebooks and 20 pens. Each notebook costs Rs. 2.25, and each pen costs Rs. 0.75. Write a C++ program that calculates the total cost of the supplies

---
+++ Main (24).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int notebook = 15;
+    int pen = 20;
+    float costofeachnotebookis = 2.25;
+    float costofeachpenis = 0.75;
+    float sum = (notebook*costofeachnotebookis + pen*costofeachpenis);
+    
+    cout<<"Total cost of notebook and pen is : "<<sum<<endl;
+    
+    
+    
+    return 0;
+}
